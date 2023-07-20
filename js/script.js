a8bfe8b2e54f389e3ea79f6b624744dc8e1d442d
aditya11201 <64565901+aditya11201@users.noreply.github.com>
2023-07-20 15:27:05 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//function dari tombol learnaboutme
+const div = document.querySelector(".btn-rounded-full.aboutme");
+div.addEventListener("click", function () {
+    window.location.href = "https://www.yourwebsite.com/about-me";
+});
+//function dari tombol learnaboutme
